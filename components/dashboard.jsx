4859cee35e7cf4c1edece27bf8e3980b15044241
Jordan <jordanboles1@gmail.com>
2022-02-24 17:19:32 -0800

    Edited conditional display

--- components/dashboard.jsx
+++ components/dashboard.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 
 import SearchBar from './searchBar';
+import Results from './results';
 
 const DashBoard = ( props ) => {
 
-  return (
-    <div className="db-wrapper">
-      <SearchBar />
-    </div>
-
-  )
+  if(props.reps.length > 0) {
+    return (
+      <div className="db-wrapper">  
+        <Results
+          reps={props.reps} 
+        />
+      </div>
+    )
+  } else {
+    return (
+      <div className="db-wrapper">  
+        <SearchBar 
+          getReps={props.getReps}
+        />
+      </div>
+    )
+  }
 }
 
 export default DashBoard;
